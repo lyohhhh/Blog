e992738f91a61fc0f3ec3fa28225a940b1addeeb
lyohhhh <lyohhhh@163.com>
2022-03-01 10:58:14 +0800

    closes: delete extra code

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <router-view></router-view>
 </template>
@@ -6,26 +5,3 @@
 <script setup lang="ts"></script>
 
 <style></style>
-=======
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
->>>>>>> 591fe90f5e8368dda875b409c6bd50a717acd24a
