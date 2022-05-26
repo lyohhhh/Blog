aeba721bdd4fc828aba079629b1bd703f0515a9e
lyohhhh <lyohhhh@163.com>
2022-05-26 10:01:32 +0800

    style: Add Dialog Translate Animate

--- src/components/dialog/src/dialog.tsx
+++ src/components/dialog/src/dialog.tsx
@@ -1,5 +1,7 @@
 import { Masker, Button } from '@/components/components';
-import { defineComponent, renderSlot, Teleport } from 'vue';
+import { defineComponent, renderSlot, Teleport, Transition } from 'vue';
+
+import dialogAnimate from '../styles/dialog.module.scss';
 
 export default defineComponent({
 	name: 'Dialog',
@@ -66,46 +68,53 @@ export default defineComponent({
 	render() {
 		return (
 			<Teleport to='body'>
-				{this.modelValue ? (
-					<div
-						class='dialog fixed bg-white rounded-md w-72 left-1/2 -translate-x-2/4 z-30 md:w-6/12 lg:w-4/12 dark:bg-themebgcolor-800'
-						style={{ top: this.top }}
-					>
-						<div class='dialog-title text-center p-4 text-lg font-semibold dark:text-themebgcolor-300'>
-							{this.$slots.title ? renderSlot(this.$slots, 'title') : '提示'}
-						</div>
-						<div class='dialog-content px-4'>
-							{this.$slots.default ? renderSlot(this.$slots, 'default') : 'content'}
-						</div>
-						<div class='dialog-footer p-4'>
-							<div class={['flex items-center', this.center ? 'justify-center' : 'justify-end']}>
-								{this.$slots.footer ? (
-									renderSlot(this.$slots, 'footer')
-								) : (
-									<>
-										{this.showCancel && (
+				<Transition
+					enterFromClass={dialogAnimate['fade-enter']}
+					leaveToClass={dialogAnimate['fade-enter']}
+					enterActiveClass={dialogAnimate['fade-active']}
+					leaveActiveClass={dialogAnimate['fade-active']}
+				>
+					{this.modelValue ? (
+						<div
+							class='dialog fixed bg-white rounded-md w-72 left-1/2 -translate-x-2/4 z-30 md:w-6/12 lg:w-4/12 dark:bg-themebgcolor-800'
+							style={{ top: this.top }}
+						>
+							<div class='dialog-title text-center p-4 text-lg font-semibold dark:text-themebgcolor-300'>
+								{this.$slots.title ? renderSlot(this.$slots, 'title') : '提示'}
+							</div>
+							<div class='dialog-content px-4'>
+								{this.$slots.default ? renderSlot(this.$slots, 'default') : 'content'}
+							</div>
+							<div class='dialog-footer p-4'>
+								<div class={['flex items-center', this.center ? 'justify-center' : 'justify-end']}>
+									{this.$slots.footer ? (
+										renderSlot(this.$slots, 'footer')
+									) : (
+										<>
+											{this.showCancel && (
+												<Button
+													loading={this.cancelLoading}
+													disabled={this.cancelDisabled}
+													onClick={this.emitCancel}
+												>
+													取消
+												</Button>
+											)}
 											<Button
-												loading={this.cancelLoading}
-												disabled={this.cancelDisabled}
-												onClick={this.emitCancel}
+												loading={this.confirmLoading}
+												disabled={this.confirmDisabled}
+												onClick={this.emitConfirm}
+												type='primary'
 											>
-												取消
+												确定
 											</Button>
-										)}
-										<Button
-											loading={this.confirmLoading}
-											disabled={this.confirmDisabled}
-											onClick={this.emitConfirm}
-											type='primary'
-										>
-											确定
-										</Button>
-									</>
-								)}
+										</>
+									)}
+								</div>
 							</div>
 						</div>
-					</div>
-				) : null}
+					) : null}
+				</Transition>
 				<Masker
 					show={this.modelValue}
 					onChange={() => {
