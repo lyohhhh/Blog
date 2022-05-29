62f07b8624e927dbe8819e58cec7b31c9a617f98
lyohhhh <lyohhhh@163.com>
2022-05-29 10:06:06 +0800

    close: close console with install component

--- src/components/install.ts
+++ src/components/install.ts
@@ -1,8 +1,6 @@
 import * as components from './components';
 import { register } from './register';
 
-console.log(Object.keys(components));
-
 export default register({
 	prefix: '',
 	components: Object.keys(components).map(key => components[key as keyof typeof components]),
